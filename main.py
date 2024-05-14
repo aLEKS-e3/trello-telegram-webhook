e885a997b3e8f9a1080394a1433052f29df9438c
aLEKS-e3 <chebukin404@gmail.com>
2024-05-14 14:51:02 +0300

    Combine all necessary functions into 1 module for convinient project launch

---
+++ main.py
@@ -0,0 +1,30 @@
+import asyncio
+import os
+import uvicorn
+
+from dotenv import load_dotenv
+
+from database import create_table
+from trello import create_board, create_trello_columns, set_trello_webhook
+from webhook import set_telegram_webhook, app
+
+load_dotenv()
+
+WEBHOOK_URL = os.getenv("WEBHOOK")
+
+
+def set_telegram_up(webhook_url: str) -> None:
+    set_telegram_webhook(webhook_url)
+
+
+def set_trello_up(webhook_url: str) -> None:
+    board_id = create_board("Board")
+    create_trello_columns(board_id, ["Done", "InProgress"])
+    set_trello_webhook(webhook_url, board_id)
+
+
+if __name__ == "__main__":
+    asyncio.run(create_table())
+    set_telegram_up(WEBHOOK_URL)
+    set_trello_up(WEBHOOK_URL)
+    uvicorn.run(app, host="0.0.0.0", port=8000)
